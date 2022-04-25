0f97a5a010e3f9b8268339e0ce2d10c1381004e5
Danil Goncharov <bigwitch3r@gmail.com>
2022-04-25 23:35:24 +0300

    Добавлены прыжки

--- main.cpp
+++ main.cpp
@@ -1,23 +1,82 @@
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
+int ground = 150;
+
+class Player
+{
+
+public:
+
+	float dx, dy;
+	FloatRect rect;
+	bool onGround;
+	Sprite sprite;
+	float currentFrame;
+
+	Player(Texture &image)
+	{
+		sprite.setTexture(image);
+		rect = FloatRect(0, 0, 24, 38);
+		dx = dy = 0;
+		currentFrame = 0;
+	}
+
+	void update(float time)
+	{
+		rect.left += dx;
+
+		if (!onGround)
+		{
+			dy = dy + 0.0005;
+		}
+
+		rect.top += dy;
+
+		onGround = false;
+
+		if (rect.top > ground)
+		{
+			rect.top = ground;
+			dy = 0;
+			onGround = true;
+		}
+
+		currentFrame += 0.005;
+
+		if (currentFrame > 6)
+		{
+			currentFrame -= 6;
+		}
+
+		if (dx > 0)
+		{
+			sprite.setTextureRect(IntRect(24 * int(currentFrame), 0, 24, 38));
+		}
+
+		if (dx < 0)
+		{
+			sprite.setTextureRect(IntRect(24 * int(currentFrame) + 24, 0, -24, 38));
+		}
+
+		sprite.setPosition(rect.left, rect.top);
+
+		dx = 0;
+	}
+
+};
 
 int main()
 {
-	RenderWindow window(VideoMode(300, 300), "Test!");
+	RenderWindow window(VideoMode(200, 200), "Test!");
 
-	CircleShape MyCircle(100.f);
-	MyCircle.setFillColor(Color::Green);
 
 	Texture texture;
 	texture.loadFromFile("walking.png");
 
 	float currentFrame = 0;
 
-	Sprite sprite;
-	sprite.setTexture(texture);
-	sprite.setTextureRect(IntRect(0, 0, 24, 38));
-	sprite.setPosition(0, 0);
+	Player player(texture);
 
 	Clock clock;
 
@@ -38,39 +97,27 @@ int main()
 
 		if (Keyboard::isKeyPressed(Keyboard::Left))
 		{
-			sprite.move(-0.1, 0);
-
-			currentFrame += 0.005;
-
-			if (currentFrame > 6)
-			{
-				currentFrame -= 6;
-			}
-
-			sprite.setTextureRect(IntRect(24 * int(currentFrame) + 24, 0, -24, 38));
+			player.dx = -0.1;
 		}
 
 		if (Keyboard::isKeyPressed(Keyboard::Right))
 		{
-			sprite.move(0.1, 0);
-
-			currentFrame += 0.005;
-
-			if (currentFrame > 6)
-			{
-				currentFrame -= 6;
-			}
-
-			sprite.setTextureRect(IntRect(24 * int(currentFrame), 0, 24, 38));
+			player.dx = 0.1;
 		}
 
 		if (Keyboard::isKeyPressed(Keyboard::Up))
 		{
-			sprite.move(0, 0.1);
+			if (player.onGround)
+			{
+				player.dy = -0.25;
+				player.onGround = false;
+			}
 		}
 
-		window.clear();
-		window.draw(sprite);
+		player.update(time);
+
+		window.clear(Color::White);
+		window.draw(player.sprite);
 		window.display();
 	}
 	return 0;
